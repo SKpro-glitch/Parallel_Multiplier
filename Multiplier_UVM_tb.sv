591df2f391708eb0f2a572498c521b56dd21816e
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-06-16 18:56:38 +0530

    Update Multiplier_UVM_tb.sv

--- Multiplier_UVM_tb.sv
+++ Multiplier_UVM_tb.sv
@@ -22,11 +22,6 @@
 import uvm_pkg::*;
 `include "uvm_macros.svh";
 
-module Parallel_Multiplier_tb(
-
-    );
-endmodule
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // This is the base transaction object that will be used in the environment to initiate new transactions and capture transactions at DUT interface
 class mult_item extends uvm_sequence_item;
@@ -69,7 +64,8 @@ class sequencer extends uvm_sequencer#(mult_item);
   endfunction
 endclass
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Allows verification components to access DUT signals using a virtual interface handle
+//Interface allows verification components to access DUT signals using a virtual interface handle
+//IF = InterFace; mult_if = Multiplier Interface
 interface mult_if (input bit clk);
   	//Clock is taken as an argument as it will be driven externally
   	//Clock can be declared inside also
@@ -82,7 +78,8 @@ interface mult_if (input bit clk);
 	logic [31:0] p;
 
 	clocking cb @(posedge clk);
-      default input #1step output #3ns;
+	   //Declaring input delay and output delay in the CLocking Block
+      default input #0ns output #0ns;
 		input p;
 		output a, b;
 	endclocking
@@ -96,41 +93,50 @@ class driver extends uvm_driver #(mult_item);
     super.new(name, parent);
   endfunction
 
-  //Virtual interface to toggle the input pins of the DUT
-  //IF = InterFace; vif = virtual interface
-  virtual mult_if vif;
+  //Virtual interface for Driver to toggle the input pins of the DUT
+  virtual mult_if vif;  //vif = virtual interface
 
   //Build phase of the Driver
+  //Build phase instantiates the components that exist inside a component 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if (!uvm_config_db#(virtual mult_if)::get(this, "", "mult_vif", vif))
+    if (!uvm_config_db#(virtual mult_if)::get(this, "", "mult_vif", vif)) //Instantiating the interface
       `uvm_fatal("DRV", "Could not get vif")
     else
       `uvm_info("DRV", $sformatf("Driver Build phase done"), UVM_LOW)
   endfunction
 
   //Run phase of the Driver
-//  virtual task run_phase(uvm_phase phase);
-//    super.run_phase(phase);
-//    forever begin
-//      mult_item m_item;
-//      `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW)
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    forever begin
+      mult_item m_item;
+      `uvm_info("DRV", $sformatf("Wait for item from sequencer"), UVM_LOW)
       
-//      //Acts as semaphore and blocks fetching of next item once item is received
-//      seq_item_port.get_next_item(m_item);
+      //Acts as semaphore and blocks fetching of next item once item is received
+      seq_item_port.get_next_item(m_item);
+
+      //Actual driving of item inputs
+      drive_item(m_item);
       
-//      //Actual driving of item inputs
-//      drive_item(m_item);
+      //Acts as semaphore and blocks the finishing of item until the work is done
+      seq_item_port.item_done();
       
-//      //Acts as semaphore and blocks the finishing of item until the work is done
-//      seq_item_port.item_done();
-//    end
-//  endtask
+      //Adding a delay of 1 clock cycle to synchronise with the Monitor
+      repeat(1) @ (posedge vif.clk);
+    end
+  endtask
 
   //Actual task to drive the input pins
   virtual task drive_item(mult_item m_item);
-  	vif.a <= m_item.a;
-    vif.b <= m_item.b;
+    @(vif.cb); 
+        //Driven by clock of the Clocking Block
+        //Must access input elements from the clocking block
+        vif.cb.a <= m_item.a;
+        vif.cb.b <= m_item.b;
+        //vif.cb.a = m_item.a   ................correct
+        //vif.a = m_item.a      ................error
+        `uvm_info("DRV", $sformatf("Inputs supplied") ,UVM_LOW);
   endtask
 endclass
 
@@ -144,43 +150,51 @@ class monitor extends uvm_monitor;
 
   //Analysis port sends data out from Monitor to be analysed
   uvm_analysis_port  #(mult_item) mon_analysis_port;
+  
+  //Virtual interface for Monitor to view outputs from the DUT
   virtual mult_if vif;
-  semaphore sema4;
-
+  
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if (!uvm_config_db#(virtual mult_if)::get(this, "", "mult_vif", vif))
+    if (!uvm_config_db#(virtual mult_if)::get(this, "", "mult_vif", vif)) //Instantiating the interface
       `uvm_fatal("MON", "Could not get vif")
     else
       `uvm_info("MON", $sformatf("Monitor Build phase done"), UVM_LOW)
-    sema4 = new(1);
     mon_analysis_port = new ("mon_analysis_port", this);
   endfunction
 
-//  virtual task run_phase(uvm_phase phase);
-//    super.run_phase(phase);
-//    // This task monitors the interface for a complete transaction and pushes into the mailbox when the transaction is complete
-//    forever begin
-//      @(posedge vif.clk);
-//      if (vif.clk) begin
-//        mult_item item = mult_item::type_id::create("item");
+  //Run phase performs the actual tasks that the class has been created for
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    // This task monitors the interface for a complete transaction and pushes into the screboard when the transaction is complete
+    forever begin
+      @(posedge vif.clk); //'Forever' block is triggered at positive edge of clock
+      
+      //Since no other control signal exists in the DUT, clock is taken as the Control signal
+      if (vif.clk) begin
+        //Item is instantiated inside the 'forever' block, so an additional 'if' block is needed
+        mult_item item = mult_item::type_id::create("item");
         
-//        //First part of transaction
-//        sema4.get();
-//        item.a = vif.a;
-//        item.b = vif.b;
-//        `uvm_info("MON", $sformatf("T=%0t [Monitor]: First part over", $time), UVM_LOW)
-//        @(posedge vif.clk);
+        //First part of transaction - supplying the input
+        item.a = vif.a;
+        item.b = vif.b;
+        `uvm_info("MON", $sformatf("T=%0t [Monitor]: First part over", $time), UVM_LOW)
         
-//        //Second part of transaction
-//		sema4.put();
-//        item.p = vif.p;        
-//        mon_analysis_port.write(item);
-//        `uvm_info("MON", $sformatf("T=%0t [Monitor]: Second part over, item:", $time), UVM_LOW)
-//        item.print();
-//      end
-//    end
-//  endtask
+        //Since output appears in the next clock cycle, a delay of 1 clock cycle is added
+        repeat (1) @ (posedge vif.clk);
+        
+        //Second part of transaction - reading the output
+		item.p = vif.p;        
+        
+        //The item is the written into the Scoreboard for matching
+        mon_analysis_port.write(item);
+        `uvm_info("MON", $sformatf("T=%0t [Monitor]: Second part over, item:", $time), UVM_LOW)
+        
+        //Item is printed for reference
+        item.print();
+      end
+    end
+  endtask
 endclass
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -191,13 +205,15 @@ class agent extends uvm_agent;
     super.new(name, parent);
   endfunction
 
-  driver 		d0; 		// Driver handle
-  monitor 		m0; 		// Monitor handle
-  sequencer     s0; 		// Sequencer Handle
+  driver 		d0;  // Driver handle
+  monitor 		m0;  // Monitor handle
+  sequencer     s0;  // Sequencer Handle
 
   //Building the respective components
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    
+    //Instantiates the Sequencer, Driver and Monitor
     s0 = sequencer::type_id::create("s0", this);
     d0 = driver::type_id::create("d0", this);
     m0 = monitor::type_id::create("m0", this);
@@ -210,6 +226,8 @@ class agent extends uvm_agent;
     super.connect_phase(phase);
     d0.seq_item_port.connect(s0.seq_item_export);
     `uvm_info("AGE", $sformatf("Agent Connect phase done"), UVM_LOW)
+    
+    //Monitor is independent of the Driver and Sequencer and is not connected
   endfunction
 endclass
 
@@ -226,17 +244,18 @@ class scoreboard extends uvm_scoreboard;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    //Instantiating the final TLM port where the Monitor will send data
     m_analysis_imp = new("m_analysis_imp", this);
     
     `uvm_info("SBD", $sformatf("Scoreboard Build phase done"), UVM_LOW)
   endfunction
 
-  //The conditions to check the results are written in this
+  //The conditions to check and validate the results are written in this
   virtual function write(mult_item item);
         if (item.p != (item.a * item.b))
           `uvm_error("SCBD", $sformatf("ERROR! Mismatch a=0x%0h b=0x%0h p=0x%0h Expected: p=0x%0h", item.a, item.b, item.p, (item.a*item.b)))
         else
-          `uvm_info("SCBD", $sformatf("PASS! Match a=0x%0h b=0x%0h p=0x%0h Expected: p=0x%0h", item.a, item.b, item.p, (item.a*item.b)), UVM_LOW)
+          `uvm_info("SCBD", $sformatf("PASS! Match a=0x%0h b=0x%0h p=0x%0h Expected: p=0x%0h", item.a, item.b, item.p, ({{16{1'b0}},item.a}*item.b)), UVM_LOW)
   endfunction
 endclass
 
@@ -253,6 +272,8 @@ class env extends uvm_env;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    
+    //Instantiating the Agent and Scoreboard
     a0 = agent::type_id::create("a0", this);
     sb0 = scoreboard::type_id::create("sb0", this);
     `uvm_info("ENV", $sformatf("Environment Build phase done"), UVM_LOW)
@@ -277,13 +298,14 @@ class gen_item_seq extends uvm_sequence;
   //Number of test cases to generate
   int num = 3;
   
-  //rand int num; 	
+  //Uncomment the following 2 lines to randomize the number of tests
+  //rand int num;
   //constraint c1 { num inside {[5:10]}; }
 
   virtual task body();
     for (int i = 0; i < num; i++) begin
     	mult_item m_item = mult_item::type_id::create("m_item");
-    	start_item(m_item);
+    	start_item(m_item); 
     	m_item.randomize(); //After this, the Driver will 'get' the item and this function will wait
     	`uvm_info("SEQ", $sformatf("Generate new item: "), UVM_LOW)
     	m_item.print();
@@ -294,7 +316,7 @@ class gen_item_seq extends uvm_sequence;
 endclass
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Instantiates an environment, sets up virtual interface handles to sub components and starts a top level sequence
+//Instantiates an environment, sets up virtual interface handles for sub components and starts a top level sequence
 class test extends uvm_test;
   `uvm_component_utils(test)
   function new(string name = "test", uvm_component parent=null);
@@ -306,51 +328,64 @@ class test extends uvm_test;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    //Instantiating the Environment
     e0 = env::type_id::create("e0", this);
     if (!uvm_config_db#(virtual mult_if)::get(this, "", "mult_vif", vif))
         `uvm_fatal("TEST", "Did not get vif")
     else
         `uvm_info("TEST", $sformatf("Test Build phase done"), UVM_LOW)
 
-      uvm_config_db#(virtual mult_if)::set(this, "e0.a0.*", "mult_vif", vif);
+    //Setting the path of the interface to inside the agent
+    uvm_config_db#(virtual mult_if)::set(this, "e0.a0.*", "mult_vif", vif);
+    //This is done instead of Connection, when connecting to a sub-component instead of an independent component
   endfunction
 
-//  virtual task run_phase(uvm_phase phase);
-//    //Top level sequence is generated
-//    gen_item_seq seq = gen_item_seq::type_id::create("seq");
+  virtual task run_phase(uvm_phase phase);
+    //Top level sequence is generated
+    gen_item_seq seq = gen_item_seq::type_id::create("seq");
     
-//    //Object is created
-//    phase.raise_objection(this);
-//    apply_reset();
+    //Object is created
+    phase.raise_objection(this);
+    //apply_reset(); //Optional reset, use if required
 
-//    seq.randomize();
-//    seq.start(e0.a0.s0);
-
-//    //Object is killed
-//    phase.drop_objection(this);
-//  endtask
+    seq.randomize();
+    seq.start(e0.a0.s0);
+    
+    //Since clock period is set to 20ns, a delay of 100ns means 5 clock cycles will be captured
+    #100;
+    
+    //Object is killed
+    phase.drop_objection(this);
+  endtask
 
   virtual task apply_reset();
     vif.rst <= 1;
-    repeat(1) @ (posedge vif.clk);
+    repeat (1) @ (posedge vif.clk);
     vif.rst <= 0;
-    repeat(1) @ (posedge vif.clk);
+    repeat (1) @ (posedge vif.clk);
   endtask
 endclass
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Top of testbench, actually starts the testbench and instantiates the design module
-module Parallel_Multiplier_tb;
+//The top module must be a 'module', not a 'class'
+module Multiplier_tb;
   reg clk, rst;
-
+  
+  //Driving the clock signal, which is passed on to all the other components
   always #10 clk = ~clk;
+  
+  //Main interface to connect the testbench to the design
   mult_if _if (clk);
 
-    Multiplier mult( .clk(clk), .rst(_if.rst), .a_in(_if.a), .b_in(_if.b));
+  //Instantiating the Design Under Test (DUT) and connecting the pins with the interface pins
+  Multiplier mult( .clk(clk), .rst(_if.rst), .a_in(_if.a), .b_in(_if.b), .p(_if.p));
 
   initial begin
     clk <= 0;
     uvm_config_db#(virtual mult_if)::set(null, "uvm_test_top", "mult_vif", _if);
+    
+    //Start the testbench
     run_test("test");
   end
 endmodule
