4afae3503c0ed28c9d6148aafdd7369cb16e27da
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-06-16 18:57:05 +0530

    Update Multiplier_UVM_tb.sv

--- Multiplier_UVM_tb.sv
+++ Multiplier_UVM_tb.sv
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12.06.2025 10:52:06
-// Design Name: 
-// Module Name: Parallel_Multiplier_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 import uvm_pkg::*;
 `include "uvm_macros.svh";
